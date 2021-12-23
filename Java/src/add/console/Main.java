f8b767863793ee352c14b04ebc8cfb66019cf28d
jin-co <lilijk@hanmail.net>
2021-12-23 22:45:52 +0900

    chore: update

--- Java/src/add/console/Main.java
+++ Java/src/add/console/Main.java
@@ -3,6 +3,14 @@ package add.console;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hi");
+        String[] participant = new String[] {"leo", "kiki", "eden"};
+        String[] participant1 = new String[] {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] participant2 = new String[] {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = new String[] {"eden", "kiki"};
+        String[] completion1 = new String[] {"josipa", "filipa", "marina", "nikola"};
+        String[] completion2 = new String[] {"stanko", "ana", "mislav"};
+        Maraton maraton = new Maraton();
+
+        System.out.println(maraton.solution(participant, completion));
     }
 }
---
+++ Java/src/add/console/Maraton.java
@@ -0,0 +1,49 @@
+package add.console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Maraton {
+    /*
+     * 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+     * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와
+     * 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+     * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+     * */
+    /*
+     * 제한사항
+     * 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+     * completion의 길이는 participant의 길이보다 1 작습니다.
+     * 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+     * 참가자 중에는 동명이인이 있을 수 있습니다.
+     * */
+
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+//        List<String> completionList = new ArrayList<>(Arrays.asList(completion));
+//        for (int i = 0; i < participant.length; i++) {
+//            if (!completionList.contains(participant[i])) {
+//                answer += participant[i];
+//            }
+//        }
+        for (int i = 0; i < participant.length; i++) {
+            for (int j = 0; j < completion.length; j++) {
+                if (participant[i] == completion[j]) {
+                    participant[i] = "";
+                    completion[j] = "";
+                }
+            }
+        }
+
+        for (String y: participant) {
+            if (y != "") {
+                answer += y;
+            }
+        }
+//        for (String k : completion) {
+//
+//        }
+        return answer;
+    }
+}

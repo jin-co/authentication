3793018518a9e55f12bac2d5ac9a2cea15bd2e2b
jin-co <lilijk@hanmail.net>
2021-12-24 22:51:34 +0900

    chore: update

---
+++ CSharp/PhoneBook/PhoneBook/PhoneBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        public static bool Phone(String[] input)
+        {
+            bool answer = true;
+
+            string[] values = input.ToString().Split(',');
+            foreach (var i in values)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+
+
+            return answer;
+        }
+    }
+}
---
+++ CSharp/PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = new string[] { "119", "97674223", "1195524421" };            
+            Console.WriteLine(PhoneBook.Phone(input));
+        }
+    }
+}

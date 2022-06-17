5cc9e7167821c15d5639f217bfb8ebf80a250e4d
jin-co <lilijk@hanmail.net>
2022-06-18 07:45:47 +0900

    Connected to db

--- app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.jinco.ecommerceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myRef = database.getReference("message")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myRef.setValue("what")
     }
 }

16cdcbaf2b8b6f06384f070b77635d6f446acc94
jin-co <lilijk@hanmail.net>
2022-06-18 08:19:06 +0900

    Added splash image

--- app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.jinco.ecommerceapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-
+//8C822677-8CC6-454F-B818-39A5CA1FEE25
 class MainActivity : AppCompatActivity() {
-    val database = Firebase.database
-    val myRef = database.getReference("message")
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myRef.setValue("what")
+
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("tests")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
